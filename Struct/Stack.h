cab2fb728fa8b289db23e9258d505d97d838f758
fpstart <123644185+fpstart@users.noreply.github.com>
2023-10-09 21:52:10 +0800

    update Struct/Stack.h

--- Struct/Stack.h
+++ Struct/Stack.h
@@ -1,13 +1,13 @@
 struct Stack
 {
-  unsigned int StackVal[128];
+  unsigned int data[128];
   struct Stack* last;
   unsigned int* top;
 }
 bool addDataToStack(struct Stack** stack,unsigned int data)
 {
   if ((**stack).top < 127){
-    
+    (**stack).data[++(**stack).top] = data;
   }else{
     
   }
