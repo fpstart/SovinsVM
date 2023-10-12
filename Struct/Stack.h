43efb07d7e55001b28d9ad5f7fe5824542bda794
fpstart <123644185+fpstart@users.noreply.github.com>
2023-10-12 19:34:28 +0800

    update Struct/Stack.h

--- Struct/Stack.h
+++ Struct/Stack.h
@@ -1,14 +1,4 @@
-struct Stack
-{
-  unsigned int data[128];
-  struct Stack* last;
-  unsigned int* top;
-}
-bool addDataToStack(struct Stack** stack,unsigned int data)
-{
-  if ((**stack).top < 127){
-    (**stack).data[++(**stack).top] = data;
-  }else{
-    
-  }
-}
+#ifndef __SOVINS_STACK_H__
+#define __SOVINS_STACK_H__
+
+#endif;
